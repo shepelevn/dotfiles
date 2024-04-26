d64ad0f6ce9d247c0f9afec638852154f66645ce
Nikolay Shepelev <shepelevnikolay97@gmail.com>
2024-04-26 13:03:28 +0300

    Move config files to the folder 'config'. Rename 'create_links.sh' as 'create-links.sh'. Add 'remove-links.sh' script. Change absolute paths to local ones in 'create-links.sh' script. Delete unused init.vim

---
+++ create-links.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ln -s $PWD/config/.vimrc ~/.vimrc
+ln -s $PWD/config/.bashrc ~/.bashrc
+ln -s $PWD/config/.inputrc ~/.inputrc
+ln -s $PWD/config/.gitconfig ~/.gitconfig
+ln -s $PWD/config/nvim ~/.config/nvim
+ln -s $PWD/config/.crontab ~/.crontab
+ln -s $PWD/config/.anacron ~/.anacron
+ln -s $PWD/config/.myclirc ~/.myclirc
+ln -s $PWD/config/.psqlrc ~/.psqlrc
---
+++ remove-links.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -i ~/.vimrc
+rm -i ~/.bashrc
+rm -i ~/.inputrc
+rm -i ~/.gitconfig
+rm -i ~/.config/nvim
+rm -i ~/.crontab
+rm -i ~/.anacron
+rm -i ~/.myclirc
+rm -i ~/.psqlrc
